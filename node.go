855a1be2bbfa02e01b12606d699d1805f4e6c6c7
Amanda Koh <amanda@naucera.net>
2018-05-15 07:08:14 +1000

    Move finding children of a node into a method.

--- node.go
+++ node.go
@@ -16,19 +16,24 @@ func (n *Node) addChild(c *Node, level int) {
 	n.children[level] = append(n.children[level], c)
 }
 
+func (n *Node) getChildrenWithinDistance(node *Node, distThreshold float64, childLevel int, dest *[]*Node) {
+	if node.Item.Distance(n.Item) <= distThreshold {
+		*dest = append(*dest, n)
+	}
+
+	for _, child := range n.children[childLevel] {
+		if node.Item.Distance(child.Item) <= distThreshold {
+			*dest = append(*dest, child)
+		}
+	}
+}
+
 func Insert(node *Node, coverSet []*Node, level int) bool {
 	distThreshold := math.Pow(2, float64(level))
 
 	var coverSetChildren []*Node
 	for _, cn := range coverSet {
-		if node.Item.Distance(cn.Item) <= distThreshold {
-			coverSetChildren = append(coverSetChildren, cn)
-		}
-		for _, child := range cn.children[level-1] {
-			if node.Item.Distance(child.Item) <= distThreshold {
-				coverSetChildren = append(coverSetChildren, child)
-			}
-		}
+		cn.getChildrenWithinDistance(node, distThreshold, level-1, &coverSetChildren)
 	}
 
 	if len(coverSetChildren) > 0 {
