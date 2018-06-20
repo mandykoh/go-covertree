c3214e19ed6ccdf144d652d25224549e48260685
Amanda Koh <amanda@naucera.net>
2018-06-20 13:14:58 +1000

    WIP Querying

--- tree.go
+++ tree.go
@@ -1,46 +1,89 @@
 package covertree
 
 import (
+	"fmt"
 	"math"
 )
 
 type Tree struct {
-	root      Item
-	rootLevel int
+	root         Item
+	rootLevel    int
+	deepestLevel int
+}
+
+func (t *Tree) FindNearest(query Item, store Store) (results []Item, err error) {
+	level := t.rootLevel
+	parentCoverSet := coverSet{t.root}
+
+	var childCoverSet coverSet
+	for level >= t.deepestLevel {
+		distThreshold := query.Distance(parentCoverSet[0])
+		for i := 1; i < len(parentCoverSet); i++ {
+			dist := query.Distance(parentCoverSet[i])
+			if dist < distThreshold {
+				distThreshold = dist
+			}
+		}
+		distThreshold += math.Pow(2, float64(level))
+
+		level -= 1
+		childCoverSet, err = parentCoverSet.child(query, distThreshold, level, store)
+		if err != nil {
+			return
+		}
+
+		parentCoverSet = childCoverSet
+	}
+
+	return childCoverSet, nil
 }
 
 func (t *Tree) Insert(item Item, store Store) error {
 	if t.root == nil {
 		t.root = item
 		t.rootLevel = math.MaxInt32
+		t.deepestLevel = t.rootLevel
 		return nil
 	}
 
 	if t.rootLevel == math.MaxInt32 {
 		t.rootLevel = levelForDistance(t.root, item)
+		t.deepestLevel = t.rootLevel
 	}
 
-	parentFound, err := insert(item, coverSet{t.root}, t.rootLevel, store)
+	parentFound, insertLevel, err := insert(item, coverSet{t.root}, t.rootLevel, store)
 
-	if !parentFound && err == nil {
-		t.root, item = item, t.root
-		t.rootLevel = levelForDistance(t.root, item)
-		_, err = insert(item, coverSet{t.root}, t.rootLevel, store)
+	if err == nil {
+		if parentFound {
+			if insertLevel < t.deepestLevel {
+				t.deepestLevel = insertLevel
+			}
+
+		} else {
+			fmt.Println("Reparenting")
+			newRootLevel := levelForDistance(item, t.root)
+
+			_, _, err = insert(t.root, coverSet{item}, newRootLevel, store)
+			if err == nil {
+				t.root = item
+				t.rootLevel = newRootLevel
+			}
+		}
 	}
 
 	return err
 }
 
-func insert(item Item, coverSet coverSet, level int, store Store) (parentFound bool, err error) {
+func insert(item Item, coverSet coverSet, level int, store Store) (parentFound bool, insertLevel int, err error) {
 	distThreshold := math.Pow(2, float64(level))
 
 	childCoverSet, err := coverSet.child(item, distThreshold, level-1, store)
 	if err != nil {
-		return false, err
+		return false, level, err
 	}
 
 	if len(childCoverSet) > 0 {
-		parentFound, err = insert(item, childCoverSet, level-1, store)
+		parentFound, insertLevel, err = insert(item, childCoverSet, level-1, store)
 		if parentFound || err != nil {
 			return
 		}
@@ -48,12 +91,12 @@ func insert(item Item, coverSet coverSet, level int, store Store) (parentFound b
 		for _, csItem := range coverSet {
 			if item.Distance(csItem) <= distThreshold {
 				err := store.Save(item, csItem, level-1)
-				return err == nil, err
+				return err == nil, level - 1, err
 			}
 		}
 	}
 
-	return false, nil
+	return false, level, nil
 }
 
 func levelForDistance(item1, item2 Item) int {
--- tree_test.go
+++ tree_test.go
@@ -3,9 +3,13 @@ package covertree
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"sort"
 	"testing"
+	"time"
 )
+
+var distanceCalls = 0
 
 type Point struct {
 	x float64
@@ -16,11 +20,12 @@ func (p Point) CoverTreeID() string {
 }
 
 func (p Point) Distance(other Item) float64 {
+	distanceCalls++
 	op := other.(*Point)
 	return math.Abs(op.x - p.x)
 }
 
-func PrintTree(item Item, level int, indentLevel int, store *InMemoryStore) {
+func PrintTree(item Item, level int, indentLevel int, store *InMemoryStore) (count int) {
 	fmt.Printf("%4d: ", level)
 	for i := 0; i < indentLevel; i++ {
 		fmt.Print("..")
@@ -30,6 +35,7 @@ func PrintTree(item Item, level int, indentLevel int, store *InMemoryStore) {
 	}
 
 	fmt.Println(item.CoverTreeID())
+	count = 1
 
 	var levels []int
 	for k := range store.levelsFor(item) {
@@ -41,9 +47,11 @@ func PrintTree(item Item, level int, indentLevel int, store *InMemoryStore) {
 		l := levels[i]
 		children, _ := store.Load(item, l)
 		for _, c := range children {
-			PrintTree(c, l, indentLevel+1, store)
+			count += PrintTree(c, l, indentLevel+1, store)
 		}
 	}
+
+	return
 }
 
 func TestSomething(t *testing.T) {
@@ -64,4 +72,88 @@ func TestSomething(t *testing.T) {
 	fmt.Println(tree.Insert(&Point{1000}, store))
 
 	PrintTree(tree.root, 10, 0, store)
+
+	distanceCalls = 0
+
+	query := &Point{6.3}
+	results, _ := tree.FindNearest(query, store)
+
+	fmt.Printf("FindNearest took %d distance calls\n", distanceCalls)
+
+	for _, r := range results {
+		fmt.Printf("Nearest: %v (distance %.1f)\n", r, r.Distance(query))
+	}
+}
+
+func TestRandom(t *testing.T) {
+	store := &InMemoryStore{}
+	tree := &Tree{}
+
+	seed := time.Now().UnixNano()
+	fmt.Println("Seed:", seed)
+	rand.Seed(seed)
+
+	values := make(map[Point]bool)
+	for i := 0; i < 10000000; i++ {
+		val := Point{rand.Float64() * 1000}
+		values[val] = true
+	}
+
+	fmt.Printf("Inserting %d values\n", len(values))
+
+	for k := range values {
+		val := k
+		err := tree.Insert(&val, store)
+		if err != nil {
+			fmt.Printf("Error inserting %v: %v\n", k, err)
+		}
+	}
+
+	//nodeCount := PrintTree(tree.root, tree.rootLevel, 0, store)
+	//fmt.Printf("Found %d nodes in tree\n", nodeCount)
+
+	query := &Point{rand.Float64() * 1000}
+	fmt.Printf("Query point %v\n", *query)
+
+	distanceCalls = 0
+	startTime := time.Now()
+
+	results, _ := tree.FindNearest(query, store)
+
+	finishTime := time.Now()
+
+	fmt.Printf("FindNearest took %d distance comparisons, %dms\n", distanceCalls, finishTime.Sub(startTime)/time.Millisecond)
+
+	for _, r := range results {
+		point := *(r.(*Point))
+		fmt.Printf("FindNearest: %v (distance %g)\n", point, r.Distance(query))
+	}
+
+	distanceCalls = 0
+	startTime = time.Now()
+
+	var nearest Point
+	var nearestDist float64
+	nearestSet := false
+
+	for k := range values {
+		val := k
+		if !nearestSet {
+			nearestSet = true
+			nearest = k
+			nearestDist = query.Distance(&val)
+
+		} else {
+			dist := query.Distance(&val)
+			if dist < nearestDist {
+				nearest = k
+				nearestDist = dist
+			}
+		}
+	}
+
+	finishTime = time.Now()
+
+	fmt.Printf("Linear Nearest took %d distance comparisons, %dms\n", distanceCalls, finishTime.Sub(startTime)/time.Millisecond)
+	fmt.Printf("Linear Nearest: %v (distance %g)\n", nearest, nearestDist)
 }
